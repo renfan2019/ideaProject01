ee662158fe02e532e66b0713b8a80805439d09da
kobe <kobe@itcast.cn>
2019-01-22 20:53:45 +0800

    克隆之后添加了一些内容

--- git_demo01/src/main/java/cn/itcast/Hello.java
+++ git_demo01/src/main/java/cn/itcast/Hello.java
@@ -3,13 +3,17 @@ package cn.itcast;
 public class Hello {
 
     public static void main(String[] args) {
+
         System.out.println("Hello");
+        System.out.println("新添加内容!!!");
     }
 
     public void test(){
 
         int i=0;
         i++;
+
+        //添加内容
 
     }
 
